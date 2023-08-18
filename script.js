18b12f0e4aeb06ff9b3b1e9fb32af5c8a63497c9
marco <119645993+vmarcoo@users.noreply.github.com>
2023-08-18 17:25:37 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector("#container");
+const dimensionText = document.querySelector("#dimensionText");
+const range = document.querySelector("#range");
+dimensionText.textContent = range.value;
+range.addEventListener("input", (event) => {
+  dimensionText.textContent = event.target.value+" x "+event.target.value;
+});
+
+function makeGrid(size) {
+if (size !== Number(size)) return console.log("Error, not a number!");
+let dimension = size*size;
+  for (i = 0; i < dimension; i++) {
+      const block = document.createElement("div");
+      container.appendChild(block);
+      block.textContent = " ";
+      block.style.backgroundColor = "brown";
+    }
+  }
+makeGrid();
