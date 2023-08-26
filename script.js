7430116a8bf0110d419f119816c800a57e02a7ad
marco <119645993+vmarcoo@users.noreply.github.com>
2023-08-26 19:45:34 -0300

    Gray-scale mode removed

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ window.onload = () => {
 }
 
 const colorBtn = document.getElementById("colorBtn");
-const grayBtn = document.getElementById("grayBtn");
 const rainbowBtn = document.getElementById("rainbowBtn");
 const eraserBtn = document.getElementById("eraserBtn");
 const clearBtn = document.getElementById("clearBtn");
@@ -26,7 +25,6 @@ colorPicker.addEventListener("input", (e)=>{colorSelection(e.target.value)})
 range.addEventListener("input", (e)=>{setGrid(e.target.value)});
 
 colorBtn.onclick = () => {setCurrentMode("color")}
-grayBtn.onclick = () => {setCurrentMode("gray")}
 rainbowBtn.onclick = () => {setCurrentMode("rainbow")}
 eraserBtn.onclick = () => {setCurrentMode("eraser")}
 clearBtn.onclick = () => {setCurrentMode("clear")}
@@ -41,32 +39,25 @@ function setStartMode (){
 function setCurrentMode(mode){
   if (mode === "color") {
     colorBtn.classList.add("activeBtn")
-    grayBtn.classList.remove("activeBtn")
     rainbowBtn.classList.remove("activeBtn")
     eraserBtn.classList.remove("activeBtn")
     currentMode = "color";
   }
-  if (mode === "gray") {
-    colorBtn.classList.remove("activeBtn")
-    grayBtn.classList.add("activeBtn")
-    rainbowBtn.classList.remove("activeBtn")
-    eraserBtn.classList.remove("activeBtn")
-    currentMode = "gray";
-  }
+
   if (mode === "rainbow") {
     colorBtn.classList.remove("activeBtn")
-    grayBtn.classList.remove("activeBtn")
     rainbowBtn.classList.add("activeBtn")
     eraserBtn.classList.remove("activeBtn")
     currentMode = "rainbow";
   } 
+
   if (mode === "eraser") {
     colorBtn.classList.remove("activeBtn")
-    grayBtn.classList.remove("activeBtn")
     rainbowBtn.classList.remove("activeBtn")
     eraserBtn.classList.add("activeBtn")
     currentMode = "eraser";
   }
+  
   if (mode === "clear") {
     clearGrid();
   }
@@ -75,18 +66,18 @@ function setCurrentMode(mode){
 function setGrid(size) {
   container.innerHTML=""
   let dimension = size*size;
-    for (i = 0; i < dimension; i++) {
-        const block = document.createElement("div");
-        block.classList.add("colorBlock");
-        block.addEventListener("mouseover", (event) => {draw(event)});
-        block.addEventListener("mousedown", (event) => {draw(event)});
-        const divSize = 500/size+"px";
-        block.style.minHeight = divSize;
-        block.style.maxHeight = divSize;
-        block.style.minWidth = divSize;
-        block.style.maxWidth = divSize;
-        container.appendChild(block);
-      }
+  for (i = 0; i < dimension; i++) {
+    const block = document.createElement("div");
+    block.classList.add("colorBlock");
+    block.addEventListener("mouseover", (event) => {draw(event)});
+    block.addEventListener("click", (event) => {draw(event)});
+    const divSize = 500/size+"px";
+    block.style.minHeight = divSize;
+    block.style.maxHeight = divSize;
+    block.style.minWidth = divSize;
+    block.style.maxWidth = divSize;
+    container.appendChild(block);
+  }
 }
 
 function clearGrid(){
@@ -103,21 +94,17 @@ function draw(color){
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
   
-    if(color.type === "mouseover" && !mouseBtnPressed) return
+  if(color.type === "mouseover" && !mouseBtnPressed) return
   
-    if(currentMode === "color"){
-      color.target.style.backgroundColor = startColor
-    }
-  
-    if(currentMode === "gray"){
-      color.target.style.backgroundColor = startColor
-    }
-  
-    if(currentMode === "rainbow"){
-      color.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
-    }
-  
-    if(currentMode === "eraser"){
-      color.target.style.backgroundColor = "#FFFFFF"
-    }
+  if(currentMode === "color"){
+    color.target.style.backgroundColor = startColor
   }
+  
+  if(currentMode === "rainbow"){
+    color.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+  }
+  
+  if(currentMode === "eraser"){
+    color.target.style.backgroundColor = "#FFFFFF"
+  }
+}
